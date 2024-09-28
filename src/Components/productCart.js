e433647eef5006f5eb9bf90b3f00b1ba4d92b2a0
Rophile Ahmed Khan <rophile.khan@gmail.com>
2024-09-29 03:12:24 +0500

    add to cart working

--- src/Components/productCart.js
+++ src/Components/productCart.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import iconCart from '../assets/images/iconCart.png';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTocart } from '../stores/cart';
 
 const ProductCart = (props) => {
+    const carts = useSelector(store => store.cart. items);
+    console.log(carts)
     const {id, name, price, image} = props.data;
+    const dispatch = useDispatch()
+    const handleAddToCart = () => {
+        dispatch(addTocart({
+            productId: id,
+            quantity: 1
+        }))
+    }
   return (
     <div className='bg-white p-5 rounded-xl shadow-sm'>
         <Link to={name}>
@@ -14,7 +25,9 @@ const ProductCart = (props) => {
             <p>
                 <span className='text-2xl font-medium'>${price}</span>
             </p>
-            <button className='bg-gray-300 p-2 rounded-md text-sm hover:bg-gray-400 flex gap-2'>
+            <button className='bg-gray-300 p-2 rounded-md text-sm hover:bg-gray-400 flex gap-2' 
+            onClick={handleAddToCart}
+            >
                 <img src={iconCart} alt='' className='w-5' />
                 Add To Cart
             </button>
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -6,7 +6,16 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-
+        addTocart(state, action){
+            const {productId, quantity} = action.payload;
+            const indexProductId = (state.items).findIndex(item => item.productId === productId)
+            if (indexProductId >= 0) {
+                state.items[indexProductId].quantity += quantity
+            }else{
+                state.items.push({productId, quantity})
+            } 
+        }
     }
 })
+export const { addTocart } = cartSlice.actions;
 export default cartSlice.reducer;
